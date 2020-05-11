7ab348c1aa900b36a38a4de1b44931a9a5ddca53
leslie.alldridge <leslie.alldridge@gmail.com>
2020-05-12 06:13:30 +1200

    init

---
+++ server.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Run when client connects
+io.on('connection', (socket) => {
+  console.log('New connection');
+});
+
+const PORT = 3000 || process.env.PORT;
+
+server.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
