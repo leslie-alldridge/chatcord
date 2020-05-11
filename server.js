7dca36519bd4b27b906875647756d60bf07cc62c
leslie.alldridge <leslie.alldridge@gmail.com>
2020-05-12 06:43:24 +1200

    messages on frontend

--- server.js
+++ server.js
@@ -11,11 +11,21 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Run when client connects
 io.on('connection', (socket) => {
-  console.log('New connection');
-
+  // Welcome current user
   socket.emit('message', 'Welcome to ChatCord!');
 
+  // Broadcast to all connections except the current connection joining
   socket.broadcast.emit('message', 'A user has joined the chat');
+
+  // User disconnecting
+  socket.on('disconnect', () => {
+    io.emit('message', 'A user has left the chat');
+  });
+
+  // Listen for chat messages
+  socket.on('chatMessage', (msg) => {
+    io.emit('message', msg);
+  });
 });
 
 const PORT = 3000 || process.env.PORT;
