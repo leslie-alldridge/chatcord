2ed2fc2c652eaff4d336ccca0ab4bdf8e6bdb053
leslie.alldridge <leslie.alldridge@gmail.com>
2020-05-12 07:06:41 +1200

    display name and time on frontend

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ io.on('connection', (socket) => {
 
   // Listen for chat messages
   socket.on('chatMessage', (msg) => {
-    io.emit('message', msg);
+    io.emit('message', formatMessage('USER', msg));
   });
 });
 
