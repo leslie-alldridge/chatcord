e5bb8e5fc34729562bd2d4a5b15669e3a678030c
leslie.alldridge <leslie.alldridge@gmail.com>
2020-05-12 07:04:36 +1200

    added user name and format for messages

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
+const formatMessage = require('./utils/messages');
 
 const app = express();
 const server = http.createServer(app);
@@ -9,17 +10,22 @@ const io = socketio(server);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+const botName = 'ChatCord Bot';
+
 // Run when client connects
 io.on('connection', (socket) => {
   // Welcome current user
-  socket.emit('message', 'Welcome to ChatCord!');
+  socket.emit('message', formatMessage(botName, 'Welcome to ChatCord!'));
 
   // Broadcast to all connections except the current connection joining
-  socket.broadcast.emit('message', 'A user has joined the chat');
+  socket.broadcast.emit(
+    'message',
+    formatMessage(botName, 'A user has joined the chat')
+  );
 
   // User disconnecting
   socket.on('disconnect', () => {
-    io.emit('message', 'A user has left the chat');
+    io.emit('message', formatMessage(botName, 'A user has left the chat'));
   });
 
   // Listen for chat messages
